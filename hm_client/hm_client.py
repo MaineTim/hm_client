0c587fcfda7128fc763359c860673c6d63b33438
Tim Sweeney <mainetim@gmail.com>
2020-01-05 17:35:43 -0500

    Implemented updates (temps only), first status view, persistent data.

--- hm_client/hm_client.py
+++ hm_client/hm_client.py
@@ -1,19 +1,77 @@
-from flask import Flask, request
+import dataset
+import datetime
+import flask
+import pprint
 
-app = Flask(__name__)
+version = "2020-01-05.01"
 
-@app.route('/update/temperature', methods=['GET','PUT','POST'])
+app = flask.Flask(__name__)
+db = dataset.connect(
+    url="sqlite:///data.db",
+    engine_kwargs={"connect_args": {"check_same_thread": False}},
+)
+
+
+# Route accepts a json string that contains update data and
+# stores it in an SQL db.
+@app.route("/update", methods=["POST"])
+def update():
+    content = flask.request.get_json()
+    table = db["temperatures"]
+    for key in content["temps"]:
+        table.upsert(
+            dict(location=key, temperature=content["temps"][key]), ["location"]
+        )
+    db["data"].upsert(
+        dict(data="time", time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
+        ["data"],
+    )
+    return flask.Response("200 OK", status=200, mimetype="application/json")
+
+
+# Route accepts a json string that contains temperature data and
+# stores it in an SQL db.
+@app.route("/update/temperature", methods=["POST"])
 def update_temps():
-    content = request.get_json()
-    print(content['temps']['basement'])
-    return "Hit!"
+    content = flask.request.get_json()
+    table = db["temperatures"]
+    for key in content["temps"]:
+        table.upsert(
+            dict(location=key, temperature=content["temps"][key]), ["location"]
+        )
+    return flask.Response("200 OK", status=200, mimetype="application/json")
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+# Route renders and returns a status page.
+@app.route("/status")
+def status():
+    temperatures = {}
+    # Create a dict of temps for rendering.
+    for entry in db["temperatures"].distinct("location", "temperature"):
+        temperatures[entry["location"]] = entry["temperature"]
+    update_time = db["data"].find_one(data="time")["time"]
+    return flask.render_template(
+        "status.html",
+        temperatures=temperatures,
+        update_time=update_time,
+        version=version,
+    )
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def catch_all(path):
-    return 'You want path: %s' % path
+    return_string = """
+<html>
+    <head>
+        <title>Housemon Ver. %s</title>
+    </head>
+    <body>
+        <p>The path you seek, /%s, is not the path to enlightenment.</p>
+    </body>
+</html>"""
+    return return_string % (version, path)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=4455)
+    app.run(host="0.0.0.0", debug=True, port=4455)
